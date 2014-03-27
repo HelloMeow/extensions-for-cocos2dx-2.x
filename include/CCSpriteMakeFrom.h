0d88ecd1982b372f6420af37c6d1e3412f87a02c
jie wen <wins2002@gmail.com>
2014-03-27 17:34:21 +0800

    add stretchable sprite creation function

--- include/CCSpriteMakeFrom.h
+++ include/CCSpriteMakeFrom.h
@@ -9,5 +9,5 @@ cocos2d::CCSprite* spriteMakeFrom(cocos2d::CCString* imagePath);
 cocos2d::CCSprite* spriteMakeFrom(const char* imagePath);
 cocos2d::CCSprite* spriteMakeFrom(cocos2d::ccColor3B color3, cocos2d::CCSize size);
 cocos2d::CCSprite* spriteMakeFrom(cocos2d::ccColor4B color4, cocos2d::CCSize size);
-
+cocos2d::CCSprite* spriteMakeFrom(const char* imagePath, cocos2d::CCSize size);
 #endif /* end of include guard: CCSPRITEMAKEFROM_H_VJDT78YS */
--- src/CCSpriteMakeFrom.cpp
+++ src/CCSpriteMakeFrom.cpp
@@ -1,4 +1,6 @@
 #include "CCSpriteMakeFrom.h"
+#include "cocos-ext.h"
+
 
 using cocos2d::CCSprite;
 using cocos2d::CCNode;
@@ -10,6 +12,9 @@ using cocos2d::ccColor4B;
 using cocos2d::CCSizeZero;
 using cocos2d::CCLayerColor;
 using cocos2d::ccc4;
+using cocos2d::extension::CCScale9Sprite;
+using cocos2d::CCSpriteBatchNode;
+using cocos2d::CCRect;
 
 CCSprite* spriteMakeFrom(CCNode* var)
 {
@@ -68,4 +73,35 @@ CCSprite* spriteMakeFrom(ccColor4B color4, CCSize size)
     if (size.equals(CCSizeZero)) return nullptr;
     
     return spriteMakeFrom(CCLayerColor::create(color4, size.width, size.height));
+}
+
+cocos2d::CCSprite* spriteMakeFrom(const char* imagePath, cocos2d::CCSize size)
+{
+    cocos2d::CCSprite* sp_original = spriteMakeFrom(imagePath);
+    
+    if (sp_original)
+    {
+        cocos2d::CCSize finalSize = CCSizeMake(MAX(size.width, sp_original->getContentSize().width),
+                                               MAX(size.height, sp_original->getContentSize().height));
+        
+        CCScale9Sprite* s = CCScale9Sprite::create();
+        if (s != NULL)
+        {
+            CCSpriteBatchNode* b = CCSpriteBatchNode::createWithTexture(sp_original->getTexture());
+            s->initWithBatchNode(b,
+                                 CCRectMake(0,
+                                            0,
+                                            sp_original->getContentSize().width,
+                                            sp_original->getContentSize().height),
+                                 CCRectMake(sp_original->getContentSize().width/2-1,
+                                            sp_original->getContentSize().height/2-1,
+                                            1,
+                                            1));
+            s->setContentSize(finalSize);
+            return spriteMakeFrom(s);
+        }
+
+    }
+    
+    return nullptr;
 }
